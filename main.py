05ee87fb8196d2e3a667c39c9be164bcd555af96
maslomuxa <shavkatovam007@gmail.com>
2025-06-27 13:52:06 +0500

    translate to russian

--- main.py
+++ main.py
@@ -24,37 +24,45 @@ class ExpenseState(StatesGroup):
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    sanitized_name = html.quote(message.from_user.full_name)
+    await message.answer(f"Привет, {html.bold(sanitized_name)}!", parse_mode="HTML")
+    await message.answer('''
+Доступные команды:
+/start - запустить программу
+/add_expense - добавить новый расход
+/show_expenses - посмотреть свои расходы
+''')
+
 
 
 
 @dp.message(Command('help'))
 async def command_help_handler(message: Message):
     await message.answer('''
-here are commands you can use:
-/start - to start a program
-/add_expense - to add an expense
-/show_expenses - to view your expenses
+Доступные команды:
+/start - запустить программу
+/add_expense - добавить новый расход
+/show_expenses - посмотреть свои расходы
 ''')
 
 
 @dp.message(Command('add_expense'))
 async def command_add_expense_handler(message: Message, state: FSMContext):
-    await message.answer("enter the date of expense (YYYY-MM-DD): ")
+    await message.answer("Введите дату расхода (ГГГГ-ММ-ДД): ")
     await state.set_state(ExpenseState.date)
 
 
 @dp.message(ExpenseState.date)
 async def date_input_handler(message: Message, state: FSMContext):
     await state.update_data(date=message.text)
-    await message.answer("Enter the category of expense: ")
+    await message.answer("Введите категорию расхода: ")
     await state.set_state(ExpenseState.category)
 
 
 @dp.message(ExpenseState.category)
 async def category_input_handler(message: Message, state: FSMContext):
     await state.update_data(category=message.text)
-    await message.answer("Enter the amount of expense: ")
+    await message.answer("Введите сумму расхода: ")
     await state.set_state(ExpenseState.amount)
 
 
@@ -69,9 +77,9 @@ async def amount_input_handler(message: Message, state: FSMContext):
         cursor.execute('INSERT INTO expenses (date, category, amount) VALUES (?, ?, ?)', (date, category, amount))
         conn.commit()
 
-        await message.answer(f"Expense added successfully! \ndate: {date} \ncategory: {category} \namount: {amount}")
+        await message.answer(f"Расход успешно добавлен! \nДата: {date} \nКатегория: {category} \nСумма: {amount}")
     except ValueError:
-        await message.answer("Enter a number for amount")
+        await message.answer("Введите число для суммы")
     finally:
         await state.clear()
 
@@ -81,9 +89,9 @@ async def amount_input_handler(message: Message, state: FSMContext):
 async def show_expenses_handler(message: Message, state: FSMContext):
     cursor.execute('SELECT * FROM expenses ORDER BY date')
     expenses = cursor.fetchall()
-    response = "your expenses: \n\n"
+    response = "Ваши расходы: \n\n"
     for expense in expenses:
-        response += f"date: {expense[1]} \ncategory: {expense[2]} \namount: {expense[3]}\n\n"
+        response += f"Дата: {expense[1]} \nКатегория: {expense[2]} \nСумма: {expense[3]}\n\n"
     await message.answer(response)
 
 
